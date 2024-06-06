c1b17429c4d942a4468915eae6a27a8413697f13
Arthur Silva <arthur.santos.off@gmail.com>
2024-06-05 22:04:16 -0300

    chore: set up sqlite, knex and validate enviroment variables with zod

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('hello', () => {
+app.get('/hello', async () => {
+  const test = await knex('sqlite_schema').select('*')
+
+  console.log(test)
+
   return 'Hello World'
 })
 
-app.listen({ port: 3333 })
+app.listen({ port: 3000 })
