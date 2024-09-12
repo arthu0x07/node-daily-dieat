8f998970523e6612bc7bb18469cac17eafd3f803
Arthur Silva <arthur.santos.off@gmail.com>
2024-09-12 01:24:44 -0300

    feat: Add token verification for user authentication in createMeals function

--- src/modules/meals/meals.service.ts
+++ src/modules/meals/meals.service.ts
@@ -4,12 +4,26 @@ import { randomUUID } from 'crypto'
 import { knex } from '@/database'
 import { MealsErrors } from './meals.errors'
 import { CreateMealsInput } from './meals.schema'
+import { FastifyJWT } from '@fastify/jwt'
 
 export async function createMeals(
   req: FastifyRequest<{ Body: CreateMealsInput }>,
   res: FastifyReply,
 ) {
   const { date, description, isOnDiet, name, userId } = req.body
+
+  const token = req.cookies.access_token
+
+  if (!token) {
+    return MealsErrors.notFoundOrInvalidPass(res)
+  }
+
+  const decoded = req.jwt.verify<FastifyJWT['user']>(token)
+  const tokenUserId = decoded.id
+
+  if (userId !== tokenUserId) {
+    return MealsErrors.notFoundOrInvalidPass(res)
+  }
 
   const createdMeals = await knex('meals').insert({
     id: randomUUID(),
