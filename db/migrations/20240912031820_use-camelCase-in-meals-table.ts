f9b031c35c2963db198501d672b2ea0c253ec4b1
Arthur Silva <arthur.santos.off@gmail.com>
2024-09-15 23:28:33 -0300

    refactor: Use consistent camelCase for column names in meals table migration

--- db/migrations/20240912031820_use-camelCase-in-meals-table.ts
+++ db/migrations/20240912031820_use-camelCase-in-meals-table.ts
@@ -2,14 +2,12 @@ import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.table('meals', (table) => {
-    table.renameColumn('userId', 'userId')
     table.renameColumn('is_on_diet', 'isOnDiet')
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
   return knex.schema.table('meals', (table) => {
-    table.renameColumn('userId', 'userId')
     table.renameColumn('isOnDiet', 'is_on_diet')
   })
 }
