d77552afbb6f32899ab95649811126c558304a3c
Arthur Silva <arthur.santos.off@gmail.com>
2024-09-26 22:22:25 -0300

    refactor: meal schema to use number instead of boolean for isOnDiet field

--- test/meals/meals.spec.ts
+++ test/meals/meals.spec.ts
@@ -51,9 +51,6 @@ describe('User Routes', () => {
     const loginToken = loginResponse.body.accessToken
     const userDecoded = app.jwt.decode(loginToken) as UserPayload | null
 
-    console.log('userDecoded', userDecoded?.id)
-    console.log('user', user.id)
-
     expect(userDecoded?.id).toEqual(user.id)
 
     expect(loginResponse.status).toEqual(200)
@@ -72,8 +69,6 @@ describe('User Routes', () => {
       .post('/meals/register')
       .set('Cookie', `access_token=${token}`)
       .send(meal)
-
-    console.log(mealResponse.error)
 
     expect(mealResponse.status).toEqual(201)
 
@@ -97,8 +92,87 @@ describe('User Routes', () => {
       .set('Cookie', `access_token=${token}`)
       .send(meal)
 
-    console.log(mealResponse.error)
+    expect(mealResponse.status).toEqual(500)
+  })
+
+  it('should not be able to create a new meal with an invalid token', async () => {
+    const token = 'invalid_token'
+
+    const meal = {
+      name: `test${randomUUID()}`,
+      userId: randomUUID(),
+      description: 'test',
+      isOnDiet: 1,
+    }
+
+    const mealResponse = await request(app.server)
+      .post('/meals/register')
+      .set('Cookie', `access_token=${token}`)
+      .send(meal)
 
     expect(mealResponse.status).toEqual(500)
+  })
+
+  it('should be able to get all meals from an existent user', async () => {
+    const { email, name, password } = {
+      email: `test${randomUUID()}@gmail.com`,
+      name: 'test',
+      password: '123456',
+    }
+
+    const registerResponse = await request(app.server)
+      .post('/users/register')
+      .send({
+        email,
+        name,
+        password,
+      })
+
+    expect(registerResponse.status).toEqual(201)
+
+    const user = await knex('users').where('email', email).first()
+
+    expect(user).toMatchObject({
+      email,
+      name,
+      password: expect.any(String),
+    })
+
+    const loginResponse = await request(app.server).post('/users/login').send({
+      email,
+      password,
+    })
+
+    const loginToken = loginResponse.body.accessToken
+
+    expect(loginResponse.status).toEqual(200)
+
+    const token = loginToken
+
+    const meal = {
+      name: `test${randomUUID()}`,
+      userId: user.id,
+      description: 'test',
+      isOnDiet: Number(true),
+    }
+
+    const mealResponse = await request(app.server)
+      .post('/meals/register')
+      .set('Cookie', `access_token=${token}`)
+      .send(meal)
+
+    expect(mealResponse.status).toEqual(201)
+
+    const mealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', `access_token=${token}`)
+
+    expect(mealsResponse.status).toEqual(200)
+
+    const meals = await knex('meals')
+      .where('userId', user.id)
+      .andWhere('name', meal.name)
+
+    expect(mealsResponse.body).toMatchObject(meals)
   })
 })
